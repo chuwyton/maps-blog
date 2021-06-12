36965c16d87c4e773b37c9a063b91c955ed9cc26
Chu Wy Ton <chuwyton@gmail.com>
2021-06-12 15:36:16 +0800

    Moved photo.vue functionality into gallery

--- docs/.vuepress/components/gallery.vue
+++ docs/.vuepress/components/gallery.vue
@@ -1,11 +1,17 @@
 <template>
-    <div class="gallery">
-        <div class="gallery-panel"
-             v-for="photo in photos"
-             :key="photo.id">
-            <router-link :to="`/gallery/photos/${photo.id}`">
-                <img :src="thumbUrl(photo.gallery, photo.filename)">
-            </router-link>
+    <div>
+        <div class="gallery">
+            <div class="gallery-panel"
+                v-for="photo in photos"
+                :key="photo.id">
+                <img :src="thumbUrl(photo.gallery, photo.filename)" v-on:click="selectPhoto(photo.id)"/>
+            </div>
+        </div>
+        <div class="lightbox" v-if="lightbox" v-on:click="closeLightbox">
+            <div class="lightbox-inner">
+                <img :src="photoUrl(selectedPhoto.gallery, selectedPhoto.filename)"/>
+                <div class="caption">{{selectedPhoto.description}}</div>
+            </div>
         </div>
     </div>
 </template>
@@ -18,8 +24,10 @@ export default {
     props: ['album'],
     data() {
         return {
-            photos
-        };
+            photos: photos,
+            lightbox: false,
+            selectedPhoto: {}
+        }
     },
     computed: {
         filteredPhotos() {
@@ -29,6 +37,19 @@ export default {
     methods: {
         thumbUrl(gallery, filename) {
             return require(`../public/pics/gallery/${gallery}/thumbnails/${filename}`)
+        },
+        photoUrl(gallery, filename) {
+            return require(`../public/pics/gallery/${gallery}/${filename}`);
+        },
+        selectPhoto(id) {
+            this.selectedPhoto = photos.find(photo => photo.id == id);
+            this.openLightbox();
+        },
+        openLightbox() {
+            this.lightbox = true;
+        },
+        closeLightbox() {
+            this.lightbox = false;
         }
     }
 }
@@ -49,5 +70,40 @@ export default {
         height: 100%;
         object-fit: cover;
         border-radius: 0.3rem;
+    }
+
+    .lightbox {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        z-index: 21;
+        background-color: rgba(0, 0, 0, 0.8);
+        display: grid;
+        grid-template-columns: 1fr 3fr 1fr;
+        grid-gap: 2rem;
+        margin-top: 0 !important;
+    }
+
+    .lightbox .lightbox-inner {
+        margin: auto;
+        width: 90vw;
+        height: 90vh;
+        grid-column-start: 2;
+    }
+
+    .lightbox .lightbox-inner img {
+        display: block;
+        margin: auto;
+        max-width: 80vw;
+        max-height: 80vh;
+
+    }
+
+    .caption {
+        padding-top: 1rem;
+        color: white;
+        text-align: center;
     }
 </style>
